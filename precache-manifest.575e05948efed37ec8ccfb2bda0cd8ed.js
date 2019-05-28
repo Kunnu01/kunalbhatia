265da06b810f33caeaa33b79828b79c7128e0f97
kunal <kunalbhatia2409@gmail.com>
2019-05-28 12:52:50 +0530

    Updates

---
+++ precache-manifest.575e05948efed37ec8ccfb2bda0cd8ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d850e8aff529a9c6739d46d91d6cb165",
+    "url": "/kunalbhatia/index.html"
+  },
+  {
+    "revision": "e0a33523bbf14dbf61b6",
+    "url": "/kunalbhatia/static/css/main.e8c8ebab.chunk.css"
+  },
+  {
+    "revision": "42d3419ee0c54fede648",
+    "url": "/kunalbhatia/static/js/2.660c0ae0.chunk.js"
+  },
+  {
+    "revision": "e0a33523bbf14dbf61b6",
+    "url": "/kunalbhatia/static/js/main.d305ac1e.chunk.js"
+  },
+  {
+    "revision": "f78cf65bd22cc5625ba9",
+    "url": "/kunalbhatia/static/js/runtime~main.21cb97c7.js"
+  }
+]);
